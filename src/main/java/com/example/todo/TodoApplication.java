d8ff00fb344ab95ee603c5eb8f77748c680c142f
dmario24 <becky2sawyer@gmail.com>
2024-04-06 06:30:54 +0900

    add MapperScan - Property 'sqlSessionFactory' or 'sqlSessionTemplate' are required
    - https://blog.naver.com/bleu1234/220256221161

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.example.todo.mapper")
 public class TodoApplication {
 
 	public static void main(String[] args) {
