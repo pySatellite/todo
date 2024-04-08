585752cff1f3a43615cbeca6209536b6087befe6
pysatellite <pysatellite@gmail.com>
2024-04-08 12:50:30 +0900

    Using @MybatisTest on JUnit 5
    - https://mybatis.org/spring-boot-starter/mybatis-spring-boot-test-autoconfigure/#using-mybatistest-on-junit-5

--- src/test/java/com/example/todo/mapper/TodoMapperTest.java
+++ src/test/java/com/example/todo/mapper/TodoMapperTest.java
@@ -2,12 +2,27 @@ package com.example.todo.mapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
+@ExtendWith(SpringExtension.class)
+@MybatisTest
 class TodoMapperTest {
+
+    @Autowired
+    private TodoMapper todoMapper;
     @Test
-    public void simpleTest() {
+    void simpleTest() {
         Assertions.assertEquals(1, 1);
     }
+
+
+    @Test
+    void findAllTest() {
+
+    }
+
 }
