e1dc970d5f9a6846cdf48b4e76b860e28a6de602
pysatellite <pysatellite@gmail.com>
2024-04-11 09:13:48 +0900

    tab size 4 -> 2

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -2,38 +2,37 @@ package com.example.todo.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.*;
-
 import com.example.todo.dto.TodoEntity;
 import com.example.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class TodoController {
+	public final TodoService todoService;
 
-    public final TodoService todoService ;
+	public TodoController(TodoService todoService) {
+		this.todoService = todoService;
+	}
 
-    public TodoController(TodoService todoService) {
-        this.todoService = todoService;
-    }
+	@GetMapping("/todos")
+	public List<TodoEntity> all() {
+		return todoService.getTodos();
+	}
 
-    @GetMapping("/todos")
-    public List<TodoEntity> all() {
-        return todoService.getTodos();
-    }
+	@GetMapping("/todos/{id}")
+	public TodoEntity find(@PathVariable Integer id) {
+		return todoService.findById(id);
+	}
 
-    @GetMapping("/todos/{id}")
-    public TodoEntity find(@PathVariable Integer id) {
-        return todoService.findById(id);
-    }
+	@PostMapping("/todos")
+	public void newTodo(@RequestBody TodoEntity todo) {
+		todoService.insert(todo);
+	}
 
-    @PostMapping("/todos")
-    public void newTodo(@RequestBody TodoEntity todo) {
-        todoService.insert(todo);
-    }
-
-    @GetMapping("/create_table")
-    public void createTable() {
-        todoService.createTable();
-    }
+	@GetMapping("/create_table")
+	public void createTable() {
+		todoService.createTable();
+	}
 }
+
