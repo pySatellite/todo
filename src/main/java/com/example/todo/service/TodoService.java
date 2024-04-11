67070b75300573292097db6aaaaf451c48b6d44c
pysatellite <pysatellite@gmail.com>
2024-04-11 09:35:48 +0900

    'member def modifier'에 잘못된 들여쓰기 수준 8이 있습니다. 예상 수준은 2여야 합니다.

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -10,26 +10,25 @@ import com.example.todo.repository.TodoMapper;
 
 @Service
 public class TodoService {
-    
-    final TodoMapper todoMapper;
+	final TodoMapper todoMapper;
 
-    public TodoService(TodoMapper todoMapper) {
-        this.todoMapper = todoMapper;
-    }
+	public TodoService(TodoMapper todoMapper) {
+		this.todoMapper = todoMapper;
+	}
 
-    public List<TodoEntity> getTodos() {
-        return todoMapper.findAll();
-    }
+	public List<TodoEntity> getTodos() {
+		return todoMapper.findAll();
+	}
 
-    public void createTable() {
-        todoMapper.createTable();
-    }
+	public void createTable() {
+		todoMapper.createTable();
+	}
 
-    public TodoEntity findById(Integer id) {
-        return todoMapper.findById(id);
-    }
+	public TodoEntity findById(Integer id) {
+		return todoMapper.findById(id);
+	}
 
-    public void insert(TodoEntity todo) {
-        todoMapper.insert(todo);
-    }
+	public void insert(TodoEntity todo) {
+		todoMapper.insert(todo);
+	}
 }
