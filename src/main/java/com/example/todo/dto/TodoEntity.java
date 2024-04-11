fdf9d3808b5cc0bcbef675d60d0955d365c33df3
pysatellite <pysatellite@gmail.com>
2024-04-11 09:26:26 +0900

    setter private

--- src/main/java/com/example/todo/dto/TodoEntity.java
+++ src/main/java/com/example/todo/dto/TodoEntity.java
@@ -1,15 +1,16 @@
 package com.example.todo.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Setter
+@Setter(AccessLevel.PRIVATE)
 @Getter
 @ToString
 public class TodoEntity {
-    private Integer  id ;
-    private String   subject ;
-    public String   body ;
-    public Boolean  completed ;
+	private Integer id;
+	private String subject;
+	public String body;
+	public Boolean completed;
 }
